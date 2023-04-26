d515043455c3e2f93b2823835f50ac703da6c858
LoickBde <loick.baude@gmail.com>
2023-04-26 23:54:22 +0200

    feat(chatbar): init chatbar feat

--- packages/frontend/src/components/Chats/Chats.tsx
+++ packages/frontend/src/components/Chats/Chats.tsx
@@ -1,3 +1,20 @@
+import { BiSearchAlt } from 'react-icons/bi';
+import { ChatCard } from '../ChatCard';
+
+const chatContact = ['Jean', 'Pierre', 'Mbappe'];
+
 export const Chats = () => {
-  return <div className="bg-green-500 w-80 h-full">Chatbar</div>;
+  return (
+    <div className="relative w-80 h-full flex flex-col">
+      <div className="w-full h-20 mb-16 p-4 flex flex-row items-center justify-between">
+        <p className="text-4xl font-semibold">Chats</p>
+        <BiSearchAlt size={32} />
+      </div>
+      <div className="grow px-2">
+        {chatContact.map((contact) => (
+          <ChatCard contactName={contact} />
+        ))}
+      </div>
+    </div>
+  );
 };
