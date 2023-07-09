dca2674697d962bae8edce77bdb883ec761d41c2
LoickBde <loick.baude@gmail.com>
2023-07-09 23:48:56 +0200

    chore(front): arrange btn and links

--- packages/front/src/components/TypingBar/TypingBar.tsx
+++ packages/front/src/components/TypingBar/TypingBar.tsx
@@ -4,24 +4,24 @@ import { BiSend } from 'react-icons/bi';
 export const TypingBar = () => {
   const [message, setMessage] = useState('');
   const sendBtnClassList = message.trim()
-    ? ' bg-ctp-blue shadow-ctp-blue/20 focus:cursor-pointe'
-    : ' bg-ctp-surface0';
+    ? ' bg-ctp-blue shadow-ctp-blue/20 focus:cursor-pointe hover:bg-ctp-blue'
+    : ' bg-ctp-surface0 hover:bg-ctp-surface0';
 
   return (
     <div className="absolute w-10/12 left-1/2 -translate-x-1/2 bottom-6 flex justify-end items-center gap-5">
       <input
         className="grow h-10 px-5 bg-ctp-surface0 text-current placeholder:text-ctp-overlay0 rounded-lg shadow-lg
         outline-offset-0 outline-none 
-        focus:outline-ctp-blue focus:shadow-ctp-blue/20 transition-all duration-300 ease-out"
+        focus:outline-ctp-blue focus:shadow-ctp-blue/20 base-transition"
         type="text"
         placeholder="Type something ..."
         value={message}
         onChange={(e) => setMessage(e.target.value)}
       ></input>
       <button
-        className={`flex shrink-0 h-10 w-10 rounded-lg transition-all duration-300 ease-out shadow-lg${sendBtnClassList}`}
+        className={`btn btn-icon shrink-0 rounded-lg shadow-lg${sendBtnClassList}`}
       >
-        <BiSend className="btn-icon" size={24} />
+        <BiSend className="btn-icon__content" size={24} />
       </button>
     </div>
   );
